0f0b857e2d23968b7dff56a7d93ffd945f945ffa
Isurumad <isurumad95@gmail.com>
2021-02-02 00:22:28 +0530

    User interface added

---
+++ main.py
@@ -0,0 +1,225 @@
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+
+def checkPrime(num):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count = count + 1
+
+    if (count == 2):
+        count = 0
+        return 1
+    else:
+        count = 0
+        return 0
+
+
+def nthPrime(num):
+    num = int(num)
+    number = 1
+    count = 0
+    temp = 1
+    while (temp):
+        if (checkPrime(number)):
+            count = count + 1
+        if (count == num):
+            print(num, " (st,nd,th) prime is ", number)
+            temp = 0
+            return number
+        number = number + 1
+
+
+def countPrime(num=0):
+    count = 0
+    num = int(num)
+    numbers = [];
+    for i in range(1, num + 1):
+        if (checkPrime(i)):
+            count = count + 1
+            numbers.append(i)
+
+    print("Has ", count, "prime numbers from 0 to ", num)
+    print(numbers)
+    return count, numbers
+
+
+def fermatNumber(num):
+    temp = 1
+    count = 1
+
+    while (temp):
+        testNum = pow(2, pow(2, count)) + 1
+        if (testNum == num):
+            print(num, " is a Fermat Number")
+            return (str(num) + " IS A FERMAT NUMBER \n")
+            temp = 0
+        elif (testNum > num):
+            temp = 0
+        else:
+            count = count + 1
+    return
+
+
+def mersenePrime(num):
+    temp = 1
+    count = 1
+    while (temp):
+        testNum = pow(2, count) - 1
+        if (testNum == num):
+            print(num, " is a Mersenne Prime number")
+            return (str(num) + " IS A MERSENNE PRIME \n")
+        elif (testNum > num):
+            temp = 0
+        else:
+            count = count + 1
+    return
+
+
+def factorialPrime(num):
+    temp = 1
+    count = 0
+    while (temp):
+        facNum1 = factorial(count) + 1
+        facNum2 = factorial(count) - 1
+
+        if (facNum1 == num or facNum2 == num):
+            print(num, " is a Factorial Prime")
+            temp = 0
+            return (str(num) + " IS A FACTORIAL PRIME \n")
+        elif (facNum1 > num):
+            temp = 0
+        else:
+            count = count + 1
+    return
+
+
+def cousinPrime(num):
+    if checkPrime(num + 4):
+        print(num + 4, " is the Cousin Prime of ", num)
+        return str(num + 4) + " IS COUSIN PRIME OF " + str(num) + "\n"
+
+    if checkPrime(num - 4):
+        print(num - 4, " is the Cousin Prime of ", num)
+        return str(num - 4) + " IS COUSIN PRIME OF " + str(num) + "\n"
+
+    return
+
+
+def sexyPrime(num):
+    if (checkPrime(num + 6)):
+        print(num + 6, " is the Sexy Prime of ", num)
+        return (str(num + 6) + " IS SEXY PRIME OF " + str(num) + "\n")
+    if (checkPrime(num - 6)):
+        print(num - 6, " is the Sexy Prime of ", num)
+        return (str(num - 6) + " IS SEXY PRIME OF " + str(num) + "\n")
+    return
+
+
+def twinPrime(num):
+    if (checkPrime(num + 2)):
+        print(num + 2, " is the Twin Prime of ", num)
+        return (str(num + 2) + " IS TWIN PRIME OF " + str(num) + "\n")
+    if (checkPrime(num - 2)):
+        print(num - 2, " is the Twin Prime of ", num)
+        return (str(num - 2) + " IS TWIN PRIME OF " + str(num) + "\n")
+    return
+
+
+def balancePrime(num):
+    previous = 0
+    after = 0
+    temp = 1
+    count = num + 1
+    for i in range(num - 1, 0, -1):
+        if (checkPrime(i)):
+            previous = i
+            break
+
+    while (temp):
+        if (checkPrime(count)):
+            after = count
+            temp = 0
+        else:
+            count = count + 1
+
+    if ((previous + after) / 2 == num):
+        print(num, " is a Balanced Prime number")
+        return (str(num) + " IS BALANCED PRIME \n")
+    else:
+        return
+    return
+
+##methods for eular functions
+def gcd (p,q):
+    while q != 0 :
+        p,q=q,p%q
+    return p
+
+def is_coprime(x,y):
+    return gcd(x,y) ==1
+
+def phi_func(x):
+    if x ==1:
+        return 1
+    else:
+        n = [y for y in range(1,x) if is_coprime(x,y)]
+        return n
+
+def eularPiFunction(num):
+    num = int(num)
+    integers = phi_func(num)
+    return integers
+
+
+def mainFunc(num):
+    i = 0
+    count = 0
+    results = []
+    num = int(num)
+    if (num >= 1):
+        if (checkPrime(num)):
+            print(num, " is a prime number")
+            results.append(str(num) + " IS A PRIME NUMBER\n")
+            if (balancePrime(num)):
+                results.append(balancePrime(num))
+            if (cousinPrime(num)):
+                results.append(cousinPrime(num))
+            if (twinPrime(num)):
+                results.append(twinPrime(num))
+            if (sexyPrime(num)):
+                results.append(sexyPrime(num))
+            if (mersenePrime(num)):
+                results.append(mersenePrime(num))
+            if (fermatNumber(num)):
+                results.append(fermatNumber(num))
+            if (factorialPrime(num)):
+                results.append(factorialPrime(num))
+            count = 0
+        else:
+            print(num, " is not a prime number")
+            results.append(str(num) + " IS NOT A PRIME\n")
+            count = 0
+    elif (num < 1 or type(num) == str):
+        print("Enter a valid number")
+    return results
+# while(1):
+#     print("********* Prime Number Calculater *********\n")
+#     print("1. Number of Prime Numbers")
+#     print("2. Check Prime")
+#     print("3. nth prime")
+#     temp = int(input("Enter your choice : "))
+#     num = int(input("Enter a number (Enter only a number) : "))
+#     if(temp == 1):
+#         countPrime(num)
+#     elif(temp == 2):
+#         mainFunc(num)
+#     elif(temp == 3):
+#         nthPrime(num)
+#     else:
+#         print("Invalid....!")
+#     print("\n")
---
+++ userInterface.py
@@ -0,0 +1,118 @@
+from tkinter import *
+from main import mainFunc, countPrime, nthPrime, eularPiFunction
+
+root = Tk()
+root.title("Prime Number Calculator")
+root.geometry("498x500")
+
+e = Entry(root, width=30, borderwidth=5)
+e.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
+Label(root, text="Insert Number : ").grid(row=0, column=5, columnspan=2, sticky=W)
+
+# output = Text(root,width=30)
+# output.grid(row=1,column=6, columnspan=5, rowspan=10)
+
+scrollbar = Scrollbar(root, orient="vertical")
+
+output = Listbox(root, width=40, height=22, yscrollcommand=scrollbar.set)
+output.grid(row=1, column=6, rowspan=10)
+scrollbar.config(command=output.yview)
+scrollbar.grid(row=1, column=7, rowspan=10, sticky=N + S)
+
+
+# scrollbar = Scrollbar(root)
+# scrollbar.grid(row=1,column=11,rowspan=10,columnspan=5)
+# output.config(yscrollcommand=scrollbar.set)
+# scrollbar.config(command=output.yview)
+
+def button_click(number):
+    current = e.get()
+    output.delete(0, END)
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+
+
+def button_clear():
+    e.delete(0, END)
+
+
+def button_option_1():
+    global number
+    number = e.get()
+    label_num = Label(root, text=number)
+    label_num.grid(row=0, column=6)
+    output.delete(0, END)
+    count, list = countPrime(number)
+    output.insert(END, ("NUMBER OF PRIMES : " + str(count) + "\n"))
+    for i in list:
+        output.insert(END, ( "  " + str(i) + "\n"))
+
+
+def button_option_2():
+    global number
+    number = e.get()
+    label_num = Label(root, text=number)
+    label_num.grid(row=0, column=6)
+    output.delete(0, END)
+    results = mainFunc(number)
+    for result in results:
+        output.insert(END, "  " + result)
+
+
+def button_option_3():
+    global number
+    global math
+    output.delete(0, END)
+    number = e.get()
+    Label(root, text=number).grid(row=0, column=6)
+    result = nthPrime(number)
+    output.insert(END, ( "  " + str(number) + "(st/nd/th) PRIME NUMBER : \n" + str(result) + "\n"))
+
+def button_option_4():
+    output.delete(0, END)
+    number = e.get()
+    Label(root, text=number).grid(row=0, column=6)
+    results = eularPiFunction(number)
+    for result in results:
+        output.insert(END, "  " + str(result))
+
+label_option = Label(root, text="Choose Option").grid(row=6, column=1, columnspan=3, pady=20)
+button_1 = Button(root, text="1", padx=20, pady=8, command=lambda: button_click(1))
+button_2 = Button(root, text='2', padx=20, pady=8, command=lambda: button_click(2))
+button_3 = Button(root, text='3', padx=20, pady=8, command=lambda: button_click(3))
+button_4 = Button(root, text='4', padx=20, pady=8, command=lambda: button_click(4))
+button_5 = Button(root, text='5', padx=20, pady=8, command=lambda: button_click(5))
+button_6 = Button(root, text='6', padx=20, pady=8, command=lambda: button_click(6))
+button_7 = Button(root, text='7', padx=20, pady=8, command=lambda: button_click(7))
+button_8 = Button(root, text='8', padx=20, pady=8, command=lambda: button_click(8))
+button_9 = Button(root, text='9', padx=20, pady=8, command=lambda: button_click(9))
+button_0 = Button(root, text='0', padx=20, pady=8, command=lambda: button_click(0))
+button_clear = Button(root, text="clear", padx=46, pady=8, command=button_clear)
+
+button_0.grid(row=5, column=1)
+
+button_option_1 = Button(root, text="Number of Primes", padx=45, pady=8, command=button_option_1)
+button_option_2 = Button(root, text="Check Prime", padx=60, pady=8, command=button_option_2)
+button_option_3 = Button(root, text="nth Prime", padx=68, pady=8, command=button_option_3)
+button_option_4 = Button(root, text="Eular Function", padx=56, pady=8, command=button_option_4)
+
+button_1.grid(row=4, column=1)
+button_2.grid(row=4, column=2)
+button_3.grid(row=4, column=3)
+
+button_4.grid(row=3, column=1)
+button_5.grid(row=3, column=2)
+button_6.grid(row=3, column=3)
+
+button_7.grid(row=2, column=1)
+button_8.grid(row=2, column=2)
+button_9.grid(row=2, column=3)
+
+button_clear.grid(row=5, column=2, columnspan=2)
+
+button_option_1.grid(row=8, column=1, columnspan=3)
+button_option_2.grid(row=9, column=1, columnspan=3)
+button_option_3.grid(row=10, column=1, columnspan=3)
+button_option_4.grid(row=11, column=1, columnspan=3)
+
+root.mainloop()
